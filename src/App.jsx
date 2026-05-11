00e2c49593e67a5255e95793550853019b07e2fb
Austin Moser <austinmosercs@gmail.com>
2026-05-11 13:19:36 -0500

    chore: update package.json scripts and add homepage; configure Vite base path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Conclusion from './pages/Conclusion/Conclusion'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/opinion" element={<Opinion />} />
